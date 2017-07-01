3f2f5819922a27f67011c20ab1f857544bb71863
pwener <phwener@gmail.com>
2017-07-01 15:10:54 -0300

    Installing twitterbootstrap gem

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get login" do
+    get users_login_url
+    assert_response :success
+  end
+
+end
