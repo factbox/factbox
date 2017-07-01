c223516ca2475d65d5e568021310d26d422ffb77
pwener <phwener@gmail.com>
2017-07-01 16:50:17 -0300

    Install rspec

--- test/controllers/users_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get login" do
-    get users_login_url
-    assert_response :success
-  end
-
-end
--- test/models/metodology_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MetodologyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
