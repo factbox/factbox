5683c3e29cc7557178e0df2a1b9021a13faba36b
pwener <phwener@gmail.com>
2017-07-01 18:44:27 -0300

    Refactor registration form to partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def create
+    @user = UrutaUser.new(user_params)
+    if @user.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def authenticate
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :lastName, :login, :password_digest)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :uruta_users, controller: 'users'
+
+  get 'projects/index'
   root 'users#index'
   post 'authenticate', to: 'users#authenticate'
 end
