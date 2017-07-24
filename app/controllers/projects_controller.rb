04b6ed73ceb57d5b603f9acdfbe2f27739366940
pwener <phwener@gmail.com>
2017-07-24 01:19:09 -0300

    Override User#projects method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,27 @@
 class ProjectsController < ApplicationController
+
+  # Used like home page of logged users
+  # GET /projects
   def index
+    # Check User#projects to see this overrided method
     @user_projects = current_user.projects
   end
 
+  # Form page to new projects
+  # GET /projects/new
   def new
     @project = Project.new
   end
 
+  # Create new projects
+  # POST /projects
   def create
     @project = Project.new(project_params)
 
     @project.author_id = current_user.id
 
     if @project.save
-      redirect_to projects_path, notice: "Pay attention to the road"
+      redirect_to projects_path, notice: "Project created successful"
     else
       render :new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :password, presence: { on: :create }, length:{ minimum: 6}, confirmation: true
 
+  # Override method that gets all projects where user belongs
+  # like membership or author
+  def projects
+    all_projects = Array.new
+    all_projects += Project.where(author_id: id)
+    all_projects += super
+
+    return all_projects
+  end
+
 end
