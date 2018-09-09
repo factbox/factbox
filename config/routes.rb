3075aa1273d122b473c981dc15203d713b7f565b
Phelipe Wener <phwener@gmail.com>
2018-09-09 15:18:40 -0300

    Adding route to edit an artifact

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
 
   get   '/projects/:id/artifacts/new', to: 'artifacts#new'
   get   '/projects/:id/artifacts/new/:type', to: 'artifacts#new_type'
+
+  get   '/artifacts/edit/:id/:type', to: 'artifacts#edit'
+
   post  '/artifacts/new', to: 'artifacts#create'
 
 end
