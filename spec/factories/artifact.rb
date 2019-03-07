71cc4ba9537d43eae4004a8ee0bca1cac9c02f6e
Phelipe <phwener@gmail.com>
2019-03-07 01:47:05 -0300

    Update attributes defined statically

--- spec/factories/artifact.rb
+++ spec/factories/artifact.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :note do
-    title "Example"
-    content "TODO"
-    author_id 7
-    project_id 1
-    actable_type 'Note'
+    title { "Example" }
+    content { "TODO" }
+    author_id { 7 }
+    project_id { 1 }
+    actable_type { 'Note' }
   end
 end
--- spec/factories/project.rb
+++ spec/factories/project.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :project do
-    name "Example"
-    description "Simple example"
+    name { "Example" }
+    description { "Simple example" }
     # It's not possible create user, because user creates this project,
     # so just quote him. See /spec/factories/user.rb
-    author_id 7
+    author_id { 7 }
   end
 end
