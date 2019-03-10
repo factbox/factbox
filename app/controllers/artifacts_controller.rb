42e2fafebe52ccaeeb1834b8504a1b3de03145d4
Phelipe <phwener@gmail.com>
2019-03-10 18:44:36 -0300

    Adjust artifacts controller to linter

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -1,49 +1,44 @@
+# Controller for manage all existing artifacts types, including plugins.
 class ArtifactsController < ApplicationController
   include ArtifactsHelper
 
   before_action :authorize, only: [:new, :new_type, :create]
   before_action :set_artifact, only: [:edit, :update]
 
-
   # Page with all artifacts type that could be created
   # GET /projects/:id/artifacts/new
   def new
-    @artifacts = Array.new
+    @artifacts = []
 
     # In production eager_load is active, but development no
     Rails.application.eager_load!
 
     ApplicationRecord.descendants.each do |artifact|
-      if artifact.is_a? Artifact
-        @artifacts.push artifact
-      end
+      @artifacts.push artifact if artifact.is_a? Artifact
     end
   end
 
   # Loads view to create selected artifact
   # GET /projects/:id/artifacts/new/:type
   def new_type
-    # The name of resource, artifact name
-    type = params[:type]
     # Used to select sources
-    @all_artifacts = Artifact.where(project_id: params[:id], version: "snapshot")
+    @all_artifacts = Artifact.where project_id: params[:id], version: 'snapshot'
 
     # Creates artifact dynamically through artifact type
     begin
-      @artifact = get_request_instance type
+      @artifact = get_request_instance params[:type]
       @artifact.project_id = params[:id]
 
-      render get_view(type, 'new'), object: @artifact
+      render get_view(params[:type], 'new'), object: @artifact
     rescue ArtifactsHelper::InvalidKlassNameError => error
       redirect_to_error_page error.message
     end
-
   end
 
   # Find artifact and render your edit page
   # GET /:type/edit/:id
   def edit
-    # TODO treat an error if this where return two ids
+    # TODO: Treat an error if this where return two ids
     # Should find first and unique artifact to get your 'specific' reference
     @artifact = Artifact.where(
       actable_type: params[:type].capitalize,
@@ -52,15 +47,19 @@ class ArtifactsController < ApplicationController
 
     # The user should not access edit page of previous versions
     # TODO maybe we could render to a new page, with more explains
-    unless @artifact.version.eql? "snapshot"
-      redirect_to controller: 'projects', action: 'show', id: @artifact.project_id
-    else
+    if @artifact.version.eql? 'snapshot'
       # The name of resource, artifact name
       @type = @artifact.actable_type.downcase
 
       @all_artifacts = Artifact.where(project_id: @artifact.project_id)
 
       render get_view(@type, 'edit')
+    else
+      redirect_to(
+        controller: 'projects',
+        action: 'show',
+        id: @artifact.project_id
+      )
     end
   end
 
@@ -77,38 +76,39 @@ class ArtifactsController < ApplicationController
     artifact_param[:author_id] = origin_artifact.author_id
     artifact_param[:project_id] = origin_artifact.project_id
 
-    if origin_artifact.version.eql? "snapshot"
+    if origin_artifact.version.eql? 'snapshot'
       origin_artifact.version = "snapshot_#{params[:id]}"
     end
 
     @artifact = get_request_instance(@type, artifact_param)
 
     if @artifact.save && origin_artifact.save
-      flash[:notice] = "Artifact updated succeed"
+      flash[:notice] = 'Artifact updated succeed'
     else
       flash[:error] = @artifact.errors || origin_artifact.errors
     end
 
-    redirect_to action: "edit", id: @artifact.id || origin_artifact.id, type: @type
+    redirect_to action: 'edit', id: @artifact.id || origin_artifact.id, type: @type
   end
 
   # Save a instance of specific artifact
   # POST /artifacts/new
   def create
-    begin
-      @artifact = get_request_instance(artifact_params[:type], artifact_params)
-      @artifact.author_id = current_user.id
+    @artifact = get_request_instance(artifact_params[:type], artifact_params)
+    @artifact.author_id = current_user.id
 
-      if @artifact.save
-        redirect_to @artifact.project
-      else
-        @all_artifacts = Artifact.where(project_id: artifact_params[:project_id], version: "snapshot")
+    if @artifact.save
+      redirect_to @artifact.project
+    else
+      @all_artifacts = Artifact.where(
+        project_id: artifact_params[:project_id],
+        version: 'snapshot'
+      )
 
-        render get_view(@artifact.actable_type, 'new'), status: 400
-      end
-    rescue ArtifactsHelper::InvalidKlassNameError => error
-      redirect_to_error_page error.message
+      render get_view(@artifact.actable_type, 'new'), status: 400
     end
+  rescue ArtifactsHelper::InvalidKlassNameError => error
+    redirect_to_error_page error.message
   end
 
   # Artifacts are not trully deleted
@@ -116,10 +116,10 @@ class ArtifactsController < ApplicationController
   def destroy
     @artifact = Artifact.find(params[:id])
 
-    @artifact.version = "deleted"
+    @artifact.version = 'deleted'
 
     if @artifact.save
-      flash[:notice] = "The artifact was deleted successfully"
+      flash[:notice] = 'The artifact was deleted successfully'
     else
       flash[:error] = @artifact.errors
     end
@@ -131,9 +131,9 @@ class ArtifactsController < ApplicationController
   # GET :project_id/artifacts/:name
   def show
     @artifact = Artifact.where(
-        project_id: params[:project_id],
-        title: params[:title]
-      ).first
+      project_id: params[:project_id],
+      title: params[:title]
+    ).first
 
     render get_view(@artifact.actable_type, 'show')
   end
@@ -149,7 +149,7 @@ class ArtifactsController < ApplicationController
     @artifact = Artifact.find(params[:id]).specific
   end
 
-  # TODO Throws error if page null
+  # TODO: Throws error if page null
   def get_view(klass, page)
     # The default of the rails views folders is lowercase and plural
     "#{klass.downcase.pluralize}/#{page}"
