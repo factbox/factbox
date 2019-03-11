dd0e0a44caff1ce80bb457b5ff660e7726d77e53
Phelipe <phwener@gmail.com>
2019-03-11 00:21:27 -0300

    Add except to artifacts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :artifacts, only: [:new, :destroy]
-  resources :notes, controller: 'artifacts'
-  resources :images, controller: 'artifacts'
+  resources :notes, controller: 'artifacts', except: [:new, :destroy]
+  resources :images, controller: 'artifacts', except: [:new, :destroy]
   resources :sessions, only: [:create, :destroy]
   resources :users
   resources :projects
