1ecbb9c2f46579879ab950a974ff8408cb2333f4
Phelipe <phwener@gmail.com>
2019-03-12 00:36:52 -0300

    Adjust model project to lint

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,12 @@
+# Model that represents the projects
 class Project < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', optional: true
   has_many :artifacts
 
-  validates :name, presence: true, length:{in: 2..20}
-  validates :description, presence: true, length:{in: 2..50}
+  validates :name, presence: true, length: { in: 2..20 }
+  validates :description, presence: true, length: { in: 2..50 }
 
   def snapshot_artifacts
-    Artifact.where(project_id: self.id, version: "snapshot")
+    Artifact.where(project_id: id, version: 'snapshot')
   end
 end
