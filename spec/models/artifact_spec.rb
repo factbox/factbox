85a38e72706dca5cb5a84360fc05541fbc420cd7
Phelipe <phwener@gmail.com>
2019-04-14 00:33:09 -0300

    Fix tests of artifact controllers

--- spec/models/artifact_spec.rb
+++ spec/models/artifact_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Artifact, type: :model do
-
-  describe "initialized in before(:all)" do
+  describe 'initialized in before(:all)' do
     before(:all) do
-      @note = FactoryBot.build(:note)
-      @note.save!
+      @note = FactoryBot.create(:note)
     end
 
-    it "has a valid factory" do
+    it 'has a valid factory' do
       expect(@note).to be_valid
     end
 
-    it "has a valid edit link" do
+    it 'has a valid edit link' do
       expect(@note.edit_link).to eq("/note/edit/#{@note.id}")
     end
   end
-
 end
