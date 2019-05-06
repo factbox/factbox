e7b0ddb4492a28a29da284cc65d56eee6527e875
Phelipe <phwener@gmail.com>
2019-05-06 02:34:01 -0300

    Add default_protect_from_forgery in config/application

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Factbox
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_controller.default_protect_from_forgery = false
   end
 end
